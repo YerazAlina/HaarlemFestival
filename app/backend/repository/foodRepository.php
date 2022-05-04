2c977201cabb166580fdd003fc0661ef8b080fcc
MhdMokhtar <m.aboudaher91@gmail.com>
2022-05-04 12:54:10 +0200

    111

--- app/backend/repository/foodRepository.php
+++ app/backend/repository/foodRepository.php
@@ -16,7 +16,7 @@ class foodRepository
     }
 
 
-    private string $all_restaurants_sql = "SELECT * FROM restaurant
+    private string $all_restaurants_sql = "SELECT * FROM foodActivity
                                       JOIN activity a on a.id = restaurantActivity.activityId
                                       RIGHT JOIN artist a2 on a2.id = restaurntActivity.artistId
                                       JOIN location l on l.id = a.locationId";
@@ -28,5 +28,54 @@ class foodRepository
         $this->stmt->setFetchMode(PDO::FETCH_CLASS, 'foodActivity');
         $this->stmt->execute();
         return $this->stmt->fetchAll();
+    }
+
+    public function findEvents(){
+
+        /*
+        if(isset($_POST["thursdayEvents"])){
+            $this->stmt = $this->db->prepare($this->thursday_events);
+
+        }
+        elseif (isset($_POST["fridayEvents"])){
+            $this->stmt = $this->db->prepare($this->friday_events);
+        }
+        elseif (isset($_POST["saturdayEvents"])){
+            $this->stmt = $this->db->prepare($this->saturday_events);
+        }
+        elseif (isset($_POST["sundayEvents"])){
+            $this->stmt = $this->db->prepare($this->sunday_events);
+        }
+        else{
+            $this->stmt = $this->db->prepare($this->all_events_sql);
+        }
+        */
+
+        $this->stmt = $this->db->prepare($this->all_events_sql);
+        $this->stmt->setFetchMode(PDO::FETCH_CLASS, 'foodActivity');
+        $this->stmt->execute();
+        return $this->stmt->rowCount();
+    }
+
+    public function purchaseTicket(){
+
+        if(isset($_POST['activityId'])){
+            $result = 'kio';
+        }
+    }
+
+    public function findById($id)
+    {
+        // TODO: Implement findById() method.
+    }
+
+    public function saveOne($object)
+    {
+        // TODO: Implement saveOne() method.
+    }
+
+    public function deleteOne($id)
+    {
+        // TODO: Implement deleteOne() method.
     }
 }
