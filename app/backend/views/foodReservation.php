7342b49a92a9c7ea3ac5bca65366cdfe45c24b41
MhdMokhtar <m.aboudaher91@gmail.com>
2022-05-23 14:11:36 +0200

    11

--- app/backend/views/foodReservation.php
+++ app/backend/views/foodReservation.php
@@ -21,54 +21,10 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light" style="background-color: #A42323">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#"> <img src="../../img/logo.png" alt="logo" width="116" height="34"> </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
-                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">FOOD</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">JAZZ</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">DANCE</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">HISTORY</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="shoppingCart">cart :)</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            English
-                        </a>
-                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Dutch</a></li>
-                        </ul>
-                    </li>
+<?php
 
-                    <li class="nav-item">
-                        <img src="">
-                    </li>
-
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">HISTORY</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+include_once ('include/navbar.php')
+?>
 
 
 
