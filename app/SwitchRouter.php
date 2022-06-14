39e609b594aa3af7d72f5e788f84f374eaecc265
YerazAlina <70819728+YerazAlina@users.noreply.github.com>
2022-06-14 19:40:06 +0200

    switchrouter stuff

--- app/SwitchRouter.php
+++ app/SwitchRouter.php
@@ -1,5 +1,7 @@
 <?php
 
+use Mollie\Api\Resources\Issuer;
+
 session_start();
 
 class SwitchRouter
@@ -7,6 +9,7 @@ class SwitchRouter
     public function route($uri, $method, $body, $path)
     {
         switch ($uri) {
+                //login/register/logout
             case '':
             case 'login':
                 if (isset($_SESSION['logged_in'])) {
@@ -15,12 +18,106 @@ class SwitchRouter
                     require __DIR__ . '/backend/views/cms/login.php';
                 }
                 break;
-            case 'home':
-                require __DIR__ . '/backend/views/cms/home.php';
+            case 'loginUser':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $controller->login();
                 break;
-            case 'foodcms':
-                require __DIR__ . '/backend/views/cms/restaurants/foodcms.php';
+            case 'registerUser':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $controller->signUp();
                 break;
+            case 'logoutUser':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $controller->logout();
+                break;
+
+
+
+
+
+
+
+
+                //personal info
+            case 'profile':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
+
+                if ($currentUserDetails->roleId >= 1) {
+                    require __DIR__ . '/backend/views/cms/users/profile.php';
+
+                    if (isset($_POST['profile'])) {
+                        $controller->updateAccount($currentUserDetails->id, $currentUserDetails->password, $currentUserDetails->roleId);
+                    }
+                } else {
+                    echo 'access denied'; 
+                }
+
+                break;
+
+            case 'updateUser':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
+                $controller->updateAccount($currentUserDetails->id, $currentUserDetails->password, $currentUserDetails->roleId);
+                break;
+
+
+
+
+
+
+
+
+
+
+
+
+                //manage users
+            case 'editAccount':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $details = $controller->getUserDetailsById($_POST['editAcc']);
+                require __DIR__ . '/backend/views/cms/users/editaccount.php';
+                break;
+            case 'registerUseraccount':
+                //for creating a new user account as superadminuser
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $controller->addUser();
+                $controller->allUsers();
+                break;
+            case 'manageUsers':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
+
+                if ($currentUserDetails->roleId >= 2) {     
+                    $controller->allUsers();
+                } else {
+                    echo 'access denied';
+                }
+
+                break;
+            case 'deleteAccount':
+                require __DIR__ . '/backend/controller/usercontroller.php';
+                $controller = new UserController();
+                $controller->deleteUser($_SESSION['email']);
+                break;
+
+
+
+
+
+
+
+
+
+                //Jazz CMS
             case 'jazzcms':
                 require __DIR__ . '/backend/controller/jazzController.php';
                 $controller = new jazzController();
@@ -30,6 +127,37 @@ class SwitchRouter
                 require __DIR__ . '/backend/controller/jazzController.php';
                 $controller = new jazzController();
                 $controller->allJazzArtistscms();
+                break;
+            case 'addArtist':
+                require __DIR__ . '/backend/controller/jazzController.php';
+                $controller = new jazzController();
+                $controller->addArtist();
+                $controller->allJazzArtistscms();
+                break;
+            case 'addEvent':
+                require __DIR__ . '/backend/controller/jazzController.php';
+                $controller = new jazzController();
+                $controller->addEvent();
+                $controller->allJazzEventscms();
+                break;
+            case 'deleteArtist':
+                require __DIR__ . '/backend/controller/jazzController.php';
+                $controller = new jazzController();
+                $controller->deleteArtist();
+                break;
+
+
+
+
+
+
+                
+                //JUST VIEWS
+            case 'home':
+                require __DIR__ . '/backend/views/cms/home.php';
+                break;
+            case 'foodcms':
+                require __DIR__ . '/backend/views/cms/restaurants/foodcms.php';
                 break;
             case 'addArtistView':
                 require __DIR__ . '/backend/views/cms/jazz/addArtist.php';
@@ -44,139 +172,10 @@ class SwitchRouter
                 //for creating a new user account as superadminuser
                 require __DIR__ . '/backend/views/cms/users/createuser.php';
                 break;
-            case 'loginUser':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $controller->login();
-                break;
-            case 'registerUser':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $controller->signUp();
-                break;
-            case 'registerUseraccount':
-                //for creating a new user account as superadminuser
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $controller->addUser();
-                $controller->allUsers();
-                break;
-            case 'addArtist':
-                //add artist to db
-                require __DIR__ . '/backend/controller/jazzController.php';
-                $controller = new jazzController();
-                $controller->addArtist();
-                $controller->allJazzArtistscms();
-                break;
-            case 'addEvent':
-                //add jazz event to db
-                require __DIR__ . '/backend/controller/jazzController.php';
-                $controller = new jazzController();
-                $controller->addEvent();
-                $controller->allJazzEventscms();
-                break;
-            case 'logoutUser':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $controller->logout();
-                break;
-            case 'profile':
-                //only alowed for users
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
-                if ($currentUserDetails->roleId >= 1) {
-                    require __DIR__ . '/backend/views/cms/users/profile.php';
-                } else {
-                    echo 'access denied'; //TODO:CHANGE THIS 
-                }
-                break;
-            case 'editAccount':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $details = $controller->getUserDetailsById($_POST['editAcc']);
-                require __DIR__ . '/backend/views/cms/users/editaccount.php';
-                break;
-            case 'manageUsers':
-                //admin can edit information from the users
-                //super admin can add, edit and delete other users
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
-
-                if ($currentUserDetails->roleId >= 2) {     //TODO THIS SHOULD BE 2
-                    $controller->allUsers();
-                    //$roles = $controller->findAllRoles(); 
-                } else {
-                    echo 'access denied'; 
-                }
-                
-                break;
             case 'manageContent':
                 require __DIR__ . '/backend/views/cms/manageContent.php';
                 break;
-            case 'deleteAccount':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $controller->deleteUser($_SESSION['email']);
-                break;
-            case 'deleteArtist':
-                require __DIR__ . '/backend/controller/jazzController.php';
-                $controller = new jazzController();
-                $controller->deleteArtist();
-                break;
-            case 'updateUser':
-                require __DIR__ . '/backend/controller/usercontroller.php';
-                $controller = new UserController();
-                $currentUserDetails = $controller->getUserDetails($_SESSION['email']);
-                $controller->updateAccount($currentUserDetails->id, $currentUserDetails->password, $currentUserDetails->roleId);
-                break;
-            case 'jazzevents':
-                require __DIR__ . '/backend/controller/jazzController.php';
-                $controller = new jazzController();
-                $controller->allJazzEvents();
 
-                //$controller = new cartController();
-                //$controller->index();
-                break;
-
-            case 'food':
-                require __DIR__ . '/backend/controller/foodController.php';
-                $controller = new foodController();
-                $controller->allfoodRestaurant();
-
-                // $controller = new cartController();
-                // $controller->index();
-                break;
-
-                // case 'foodReservation':
-                //     require __DIR__ . '/backend/controller/foodController.php';
-                //     $controller = new foodController();
-                //     $controller->allfoodRestaurant();
-
-                //     $controller = new cartController();
-                //     $controller->index();
-                //     break;
-
-            case 'cart':
-                require __DIR__ . '/backend/controller/cartController.php';
-                $controller = new cartContoller();
-                $controller->addToCart();
-                break;
-            case 'jazzevents':
-                require __DIR__ . '/backend/controller/jazzController.php';
-                $controller = new jazzController();
-                $controller->allJazzEvents();
-
-                //frequire __DIR__ . '/backend/controller/cartController.php';
-                //$controller = new cartController();
-                //$controller->index();
-                break;
-            case 'cart':
-                require __DIR__ . '/backend/controller/cartController.php';
-                $controller = new cartContoller();
-                $controller->addToCart();
-                break;
             default:
                 echo '404 not found';
                 http_response_code(404);
